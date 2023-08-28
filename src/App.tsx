3164af144c7cd12f176ab4a57868125cde10a4f4
CodeSandbox <support@codesandbox.io>
2023-08-28 10:30:34 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React + Vite</h1>
       <h2>On CodeSandbox!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
